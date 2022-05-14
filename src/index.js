fae3920361e13be4b287943ad5a08ae8c33115ac
SyedTayyabUlMazhar <stayyabmazhar19991@gmail.com>
2022-05-14 13:07:16 +0500

    Added react-navigation library.

---
+++ src/index.js
@@ -0,0 +1,14 @@
+import 'react-native-gesture-handler';
+import React from 'react';
+import { View, Text, Image } from 'react-native';
+// import styles from './styles';
+
+
+export default App = () =>
+{
+  return (
+    <View style={{flex:1, backgroundColor:'powderblue', justifyContent:'center', alignItems:'center'}}>
+      <Text>Hellow World</Text>
+    </View>
+  );
+};
